c8b520676d4843535418427c75f0a5da0d91a5c9
mavegituser <livegit2@gmail.com>
2021-01-07 11:59:24 +0530

    .

---
+++ java/Developer.java
@@ -0,0 +1,20 @@
+// Developer is the class
+// main() is a method or function
+// source code : the code typed by the developer.
+public class Developer {
+	public static void main(String args[]){
+	System.out.println("Full Stack Developers");
+	}
+}
+//Developer.java is source code
+//compile the program using the javac (compiler)
+//verify Developer.class (bytecode)
+//run the byte code using JVM.
+// java Developer.
+
+//Error : Error: Could not find or load main class Developer
+//set classpath=%classpath%;.
+
+//execution of the program
+//C:\Users\Enzo\Desktop>java Developer
+//Full Stack Developers
